f0b772a0c451d07673bbbc81e8baa321ebe39cbe
23120312 <chanelhynvuigames@gmail.com>
2025-10-19 20:24:22 +0700

    Gắn sự kiện click để đóng mở (thu gọn) NEWS

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  // Lắng nghe sự kiện click trên mỗi '.news-header'
+  $(".toggle-icon").on("click", function () {
+    // Tìm 'li.news-item' cha gần nhất của header vừa được click
+    const newsItem = $(this).closest(".news-item");
+
+    // Tìm icon và nội dung bên trong item đó
+    const toggleIcon = newsItem.find(".toggle-icon");
+    const newsContent = newsItem.find(".news-content");
+
+    // Dùng slideToggle() để tạo hiệu ứng trượt lên/xuống cho nội dung
+    newsContent.slideToggle();
+
+    // Thêm/xóa class 'expanded' trên 'li.news-item'
+    newsItem.toggleClass("expanded");
+
+    // Kiểm tra xem item có class 'expanded' không để đổi icon
+    if (newsItem.hasClass("expanded")) {
+      toggleIcon.text("▼"); // Nếu đang mở, hiện icon down
+      newsContent.css("display", "block"); // Sửa thành .css("display", "block")
+    } else {
+      toggleIcon.text("▶"); // Nếu đang đóng, hiện icon right
+      newsContent.css("display", "none"); // Sửa thành .css("display", "none")
+    }
+  });
+});
